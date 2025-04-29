04ec8e2583ca7761f4b7b093e8e5e3404bc84702
AKHIL ROYAL <akhilroyal5187@gmail.com>
2025-04-29 17:35:54 +0530

    Update verifyAdmin.js

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -24,4 +24,11 @@ const verifyAdmin = (req, res, next) => {
     }   
 };
 
-export default verifyAdmin;
+export const authoriseRoles = (roles)=>{
+    return(req, res, next)=>{
+        if(!roles.includes(res.user.roles)){
+            return res.status(403).json({message : "Access denied : Admins only"});
+        }
+        next();
+    }
+}   
